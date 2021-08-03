711e763a606228a1be058efc8680bdbaa43fb4d8
Giovanni <casolegiovanni@gmail.com>
2021-08-03 18:27:29 +0200

    some refactoring 3part

--- js/alt-script.js
+++ js/alt-script.js
@@ -1,22 +1,12 @@
 let matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
+var sections = document.getElementsByClassName("col-mode");
 
-var r = document.querySelector(":root");
-
-function setupMetaElement(property, value, content) {
+function setupMetaElement(id, id_value, property, value, content) {
   var meta = document.createElement("meta");
+  meta.setAttribute(id, id_value);
   meta.setAttribute(property, value);
   meta.content = content;
   document.getElementsByTagName("head")[0].appendChild(meta);
-}
-
-function set_colors() {
-  if (r.classList.contains("dark")) {
-    r.classList.remove("dark");
-    r.classList.add("light");
-  } else {
-    r.classList.remove("light");
-    r.classList.add("dark");
-  }
 }
 
 function btn_change() {
@@ -30,27 +20,11 @@ function btn_change() {
 }
 
 if (matched) {
-  r.classList.add("dark");
-  setupMetaElement("name", "theme-color", "#04002e");
+  setupMetaElement("id", "theme-color", "name", "theme-color", "#04002e");
 } else {
-  r.classList.add("light");
-  setupMetaElement("name", "theme-color", "#aba3ff");
+  setupMetaElement("id", "theme-color", "name", "theme-color", "#aba3ff");
 }
 
 function switch_color_scheme() {
-  // set_colors();
   btn_change();
 }
-
-
-
-
-// r.style.setProperty("--main-background-color", "hsl(245, 30%, 9%)");
-// r.style.setProperty("--secondary-background-color", "hsl(245, 30%, 18%)");
-// r.style.setProperty("--main-text-color", "hsl(245, 30%, 82%)");
-// r.style.setProperty("--secondary-text-color", "hsl(245, 30%, 91%)");
-
-// r.style.setProperty("--main-text-color", "hsl(245, 30%, 9%)");
-// r.style.setProperty("--secondary-text-color", "hsl(245, 30%, 18%)");
-// r.style.setProperty("--main-background-color", "hsl(245, 30%, 82%)");
-// r.style.setProperty("--secondary-background-color", "hsl(245, 30%, 91%)");
